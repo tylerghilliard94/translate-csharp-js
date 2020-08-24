8da9c75b9f3f2ca730419401b7931ea74f581b83
Tyler Grey Hilliard <tylerghilliard@gmail.com>
2020-08-24 11:48:07 -0500

    part2 exercise 3

---
+++ fizzbuzz/csharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void FizzBuzz()
+            {
+
+
+                for (int n = 1; n <= 100; n++)
+                {
+                    if (n % 15 == 0)
+                    {
+                        Console.WriteLine("fizzbuzz");
+                    }
+                    else if (n % 3 == 0)
+                    {
+                        Console.WriteLine("fizz");
+                    }
+                    else if (n % 5 == 0)
+                    {
+                        Console.WriteLine("buzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine(n);
+                    }
+                }
+            }
+            FizzBuzz();
+
+        }
+    }
+}
